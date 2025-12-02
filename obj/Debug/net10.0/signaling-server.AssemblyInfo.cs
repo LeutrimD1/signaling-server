4dd9e7eadec62db3c653f334c92e74597d0ece37
Leutrim Dema <LeutrimD1@gmail.com>
2025-12-02 12:48:21 -0600

    update

--- obj/Debug/net10.0/signaling-server.AssemblyInfo.cs
+++ obj/Debug/net10.0/signaling-server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("signaling-server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3448321c3553fdfc1e9a58a9ee1f7b70d22c4401")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e90398804c34343af3c940a6c18b86938d7a90bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("signaling-server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("signaling-server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
